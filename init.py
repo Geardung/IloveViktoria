584c16a1811688502c17a34de052bb5ef99c3505
Geardung <Geardung@ya.ru>
2024-08-28 01:52:51 +0300

    ячс

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
 import os
 
 THREADS_FOLDER_NAME = "threads"
+TEMP_FOLDER_NAME = "temp"
 
 
 #
 THREADS_FOLDER_PATH = "./" + THREADS_FOLDER_NAME + "/"
+TEMP_FOLDER_PATH = "./" + TEMP_FOLDER_NAME + "/"
 
 
 if not os.path.exists(THREADS_FOLDER_PATH): os.mkdir(THREADS_FOLDER_PATH)
+if not os.path.exists(TEMP_FOLDER_PATH): os.mkdir(TEMP_FOLDER_PATH)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import asyncio
-import init
+import asyncio, openai, ujson, base64, requests
+from random import randint
+from datetime import datetime
+
+from init import THREADS_FOLDER_PATH, TEMP_FOLDER_PATH
 
 from aiogram import Bot, Dispatcher, html, Router, F
 from aiogram.client.default import DefaultBotProperties
@@ -7,23 +10,56 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, callback_data
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.utils.markdown import link
+from aiogram.utils import chat_action
 
-from config import TG_TOKEN
+import asyncio
+from openai_streaming import process_response
+from typing import AsyncGenerator
+
+from config import TG_TOKEN, GPT_TOKEN
+
+print("ZHOPA")
 
 bot = Bot(token=TG_TOKEN, default=DefaultBotProperties())
-
 dp = Dispatcher()
-
 main_router = Router()
+
+
+client = openai.AsyncOpenAI(
+    api_key=GPT_TOKEN,
+    base_url="https://lk.neuroapi.host/v1"
+)
+
 
 class MyCallback(callback_data.CallbackData, prefix="vikasex"):
     state: str
-
 
 async def main() -> None:
     
     await dp.start_polling(bot)
 
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# def generate_response(messages: list[dict[str, str]],
+#                       model = "gpt-4o-mini"
+#                       ):
+    
+#         _ = client.chat.completions.create(
+#             model=model,
+#             max_tokens=300,
+#             messages=messages,
+#             presence_penalty=0,
+#             #stream=True,
+#             temperature=0.5,
+#             top_p=1,
+#             frequency_penalty=0)
+        
+#         print(_)
+        
+#         return _.choices
 
 @main_router.callback_query(MyCallback.filter(F.state == "create_new_topic"))
 async def create_topic_callback(query: CallbackQuery, callback_data: MyCallback): 
@@ -35,6 +71,13 @@ async def create_topic_callback(query: CallbackQuery, callback_data: MyCallback)
     new_topic = await query.message.bot.create_forum_topic(query.message.chat.id, "Новый чат")
     
     await query.answer()
+    
+    with open(THREADS_FOLDER_PATH + str(new_topic.message_thread_id) + ".json", "w", encoding="utf-8") as f:
+        
+        f.write(ujson.dumps({
+            "engine-selected-now": "gpt-4o-mini"
+        }))
+        
     
     msg = await query.message.bot.send_message(
         text="Солнышко, если хочешь удалить этот чат, ты можешь воспользоваться кнопкой снизу\nТакже, кнопкой можно выбрать модель для конкретного чата!",
@@ -48,32 +91,11 @@ async def create_topic_callback(query: CallbackQuery, callback_data: MyCallback)
                                     reply_markup=markup_keyboard,
                                     parse_mode="MARKDOWN")
     
-    await asyncio.sleep(15)
+    await asyncio.sleep(5)
     
     await dl_msg.delete()
     
     await msg.pin()
-    
-    
-    
-@main_router.message(Command("new"))
-async def create_topic(message: Message):
-    
-    markup_keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="GPT-4o-mini", callback_data=MyCallback(state="select_model_gpt4o-mini").pack())],
-                                                            [InlineKeyboardButton(text="GPT-4o", callback_data=MyCallback(state="select_model_gpt4o").pack())],
-                                                            [InlineKeyboardButton(text="Удалить чат", callback_data=MyCallback(state="delete_this_topic").pack())]])
-    
-    new_topic = await message.bot.create_forum_topic(message.chat.id, "Новый чат")
-    
-    msg = await message.bot.send_message(
-        text="Солнышко, если хочешь удалить этот чат, ты можешь воспользоваться командой /delete\nТакже, если хочешь изменить модель для этого чата, воспользуйся кнопками ниже!",
-        reply_markup=markup_keyboard,
-        chat_id=message.chat.id,
-        message_thread_id=new_topic.message_thread_id)
-    
-    await msg.pin()
-    
-    await message.delete()
 
 @main_router.callback_query(MyCallback.filter(F.state == "delete_this_topic"))
 async def callback_query_handler(query: CallbackQuery, callback_data: MyCallback): 
@@ -81,23 +103,6 @@ async def callback_query_handler(query: CallbackQuery, callback_data: MyCallback
     await bot.delete_forum_topic(query.message.chat.id,
                                  query.message.message_thread_id)
     
-@main_router.message(Command("adm-reg"))
-async def deererrelete_topic(message: Message):
-    
-    markup_keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Новый чат", callback_data=MyCallback(state="create_new_topic").pack())]])
-    
-    msg = await message.bot.send_message(
-        text="Викуличка любимая, чтобы создать новый чат для диалога с ботом, нажми на кнопку снизу",
-        reply_markup=markup_keyboard,
-        chat_id=message.chat.id)
-    
-    await msg.pin()
-    
-    await message.delete()
-
-    
-
-
 @main_router.message(Command("adm-thread-id"))
 async def delete_topic(message: Message):
     
@@ -105,22 +110,64 @@ async def delete_topic(message: Message):
         f"{message.message_thread_id}"
                         )
 
-@main_router.message(Command("adm-delete"))
-async def delete_topic(message: Message):
-    
-    await bot.delete_forum_topic(message.chat.id,
-                                 message.message_thread_id)
-
-
 @main_router.message()
 async def any_topic_message(message: Message):
     
     if not message.chat.is_forum: return
-    elif not message.message_thread_id in [None, 3]: return
+    elif message.message_thread_id in [None, 3]: return
     elif message.from_user.is_bot: return
     
+    await bot.send_chat_action(chat_id=message.chat.id,
+                         action="typing",
+                         message_thread_id=message.message_thread_id,
+                         )
     
+    #image_path = ".\SourceImages\some_text.jpg"
+    #base64_image = encode_image(image_path)
     
+    imgs = []
+    
+    last_message_dict = {"role": "user", "name": "Викуличка", 
+         "content": [ {"type": "text", "text": message.text}] + [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}} for base64_image in imgs]}
+    
+    print([{"role": "user", "content": message.text or message.caption}])
+    
+    photo_pathes = []
+    _ = TEMP_FOLDER_PATH+str(randint(0, 9999999999))+"_"+str(int(datetime.now().timestamp())) + "_" + str(message.message_thread_id) + ".jpg"
+    photo_pathes.append(_)
+    await bot.download(message.photo[-1], _)
+        
+    photo_promt = [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encode_image(img)}"}} for img in photo_pathes]
+    
+    async def content_handler(content: AsyncGenerator[str, None]):
+        async for token in content:
+            print(token, end="")
+            
+    resp = await client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": [{"type": "text", "text": message.text or message.caption}] + photo_promt}],
+        stream=True
+    )
+    await process_response(resp, content_handler)
+    
+            
+    
+    # await message.reply(generate_response([{"role": "system", "content": "Ты умная лучшая подруга моей девушки Вики."}, 
+    #                                        {"role": "user", "content": [{"type": "text", "text": message.text or message.caption}] + photo_promt}])\
+                                               
+                                               
+    #                                            [0].message.content.replace(".", "\.").replace("(", "\(").replace("-", "\-"),
+    #                     parse_mode="MarkdownV2")
+
+
+
+# {
+    # “type”: “image_url”,
+    # “image_url”: {
+    #   “url”: f"data:image/jpeg;base64,{base64_image}"
+#     }
+# }
+
 
 dp.include_router(main_router)
 
